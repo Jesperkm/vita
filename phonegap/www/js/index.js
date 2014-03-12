2e40797611679d945358d91f5335dc58c1be0901
Tony Dieu <tnydieu@gmail.com>
2014-03-12 20:15:09 +0100

    add zepto 0.1 project

--- phonegap/www/js/index.js
+++ phonegap/www/js/index.js
@@ -1,49 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+window.addEventListener('load', function() {
+    FastClick.attach(document.body);
+}, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+Zepto(function($) {
+    function getWord() {
+        // Check if input field is empty.
+        if ($('#addWord').val() == null) {
+            alert("skriv venligst en tekst");
+        } else {
+            // Click event for the class word.
+            $('.btn-click').on('click', function(e) {
+                // get word scope withing scope.
+                var word = $(this).text();
+                // add html to results
+                $('#textfield').append('<li class="' + word + ' result" "><a href="#" class="item">X  ' + word + ' </a></li>');
+            });
+            // click event for input field.
+            $(document).on('click', '#submit_word', function() {
+                if ($('#addWord').val() == "") //
+                {
+                    alert("skriv venligst en tekst");
+                } else {
+                    // output string from input field.
+                    $('#textfield').append('<div class="result" "><a href="#" class="item">X ' + $('#addWord').val() + '</a></div>');
+                }
+            });
+            // remove word from result list.
+            $(document).on('click', '.result', function() {
+                $(this).remove();
+            });
+            $(document).on('click', '#trashcan', function() {
+                // remove all results.
+                $('.result').remove();
+            });
+        }
+    };
 
-        console.log('Received Event: ' + id);
-    }
-};
+    getWord();
+
+})
